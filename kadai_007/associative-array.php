734d0c11173d8a9a6995ff71fd745ed865c94965
chikiban <110600157+chikiban@users.noreply.github.com>
2024-03-17 14:55:55 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=p, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $Array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    
+    print_r($Array);
+
+    ?>
+  </p>
+</body>
+
+</html>
