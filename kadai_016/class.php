bf5f489e2a8da5b7ec343e51f542d84e96448ec7
chikiban <110600157+chikiban@users.noreply.github.com>
2024-04-06 18:59:38 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    class Food {
+     private $name;
+     private $price;
+
+      public function __construct(string $name,int $price) {
+        $this->name = $name;
+        $this->price = $price;
+      }
+      public function show_price() {
+        echo $this->price . '<br>';
+      }
+    }
+
+    $Food = new Food('potato',250);
+
+    print_r($Food);
+
+    echo '<br>';
+
+    class Animal {
+      private $name;
+      private $height;
+      private $weight;
+
+      public function __construct(string $name,int $height,int $weight) {
+        $this->name=$name;
+        $this->height=$height;
+        $this->weight=$weight;
+      }
+      public function show_height() {
+        echo $this->height . '<br>';
+      }
+    }
+
+    $Animal = new Animal('dog',60,5000);
+
+    print_r($Animal);
+
+    echo '<br>';
+
+    $Food->show_price();
+    $Animal->show_height();
+    ?>
+  </p>
+</body>
+</html>
